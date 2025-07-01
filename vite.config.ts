b6c917a8ae9d37a36523ef2fd7be6671fcef7143
AndriyNyk0lyn <andriy.nykolyn@bitsorchestra.com>
2025-07-01 21:38:20 +0300

    Add HTTPS support in Vite configuration with local certificates

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,5 +19,12 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+    },
+    port: 3000,
   },
 });
